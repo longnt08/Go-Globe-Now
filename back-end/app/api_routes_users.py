a677b3a54f5f946ed5ac7c55bc43722f8007c25e
vanh7979 <137498913+vanh7979@users.noreply.github.com>
2024-11-04 16:19:18 +0700

    test

--- back-end/app/api_routes_users.py
+++ back-end/app/api_routes_users.py
@@ -1,20 +1,28 @@
 from flask import Blueprint, request, jsonify
+from pymongo import MongoClient
+from bson import ObjectId
 
+# Tạo một Blueprint mới
 users_api = Blueprint('users_api', __name__)
 
-# Giả sử bạn có một danh sách người dùng ở đây
-users = {
-    1: {"name": "Nguyễn Văn A", "email": "a@example.com"},
-    2: {"name": "Trần Thị B", "email": "b@example.com"},
-}
+# Kết nối đến MongoDB Atlas
+client = MongoClient("mongodb+srv://thanhlong:LTWNhom3@goglobenow.vroew.mongodb.net/")
+db = client.people  # Chọn cơ sở dữ liệu 'people'
+users_collection = db.users  # Chọn collection 'users'
+
 @users_api.route('/', methods=['GET'])
 def get_users():
+    # Lấy tất cả người dùng từ collection 'users'
+    users = list(users_collection.find({}))
+    for user in users:
+        user['_id'] = str(user['_id'])  # Chuyển đổi ObjectId thành chuỗi
     return jsonify(users), 200
 
-@users_api.route('/<int:user_id>', methods=['PUT'])
+@users_api.route('/<user_id>', methods=['PUT'])
 def update_user(user_id):
     # Kiểm tra xem người dùng có tồn tại không
-    if user_id not in users:
+    user = users_collection.find_one({"_id": ObjectId(user_id)})  # Sử dụng ObjectId để tìm
+    if user is None:
         return jsonify({"message": "User not found"}), 404
     
     # Lấy dữ liệu từ yêu cầu
@@ -22,8 +30,23 @@ def update_user(user_id):
 
     # Cập nhật thông tin người dùng
     if 'name' in data:
-        users[user_id]['name'] = data['name']
+        users_collection.update_one({"_id": ObjectId(user_id)}, {"$set": {"name": data['name']}})
     if 'email' in data:
-        users[user_id]['email'] = data['email']
+        users_collection.update_one({"_id": ObjectId(user_id)}, {"$set": {"email": data['email']}})
 
-    return jsonify({"message": "User updated", "user": users[user_id]}), 200
+    # Lấy thông tin người dùng đã cập nhật
+    updated_user = users_collection.find_one({"_id": ObjectId(user_id)})
+    return jsonify({"message": "User updated", "user": updated_user}), 200
+
+@users_api.route('/', methods=['POST'])
+def add_user():
+    data = request.get_json()  # Nhận dữ liệu từ yêu cầu
+    new_user = {
+        "name": data['name'],
+        "email": data['email'],
+    }
+    users_collection.insert_one(new_user)  # Thêm người dùng mới vào collection
+    new_user['_id'] = str(new_user['_id'])  # Chuyển đổi ObjectId thành chuỗi để trả về
+    return jsonify({"message": "User added", "user": new_user}), 201
+
+
